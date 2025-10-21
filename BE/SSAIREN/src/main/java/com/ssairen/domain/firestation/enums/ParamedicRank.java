70711e8c18c129cafd7b60f486add96fe2a61bb1
pong0882 <parkdanil@naver.com>
2025-10-21 15:04:59 +0900

    fix: pramedicRank enum 재확인 후 수정 (Closed S13P31A205-59)

--- BE/SSAIREN/src/main/java/com/ssairen/domain/firestation/enums/ParamedicRank.java
+++ BE/SSAIREN/src/main/java/com/ssairen/domain/firestation/enums/ParamedicRank.java
@@ -1,9 +1,16 @@
 package com.ssairen.domain.firestation.enums;
 
 public enum ParamedicRank {
-    FIREFIGHTER,
-    SENIOR_FIREFIGHTER,
-    FIRE_SERGEANT,
-    FIRE_LIEUTENANT,
-    FIRE_CAPTAIN
+    소방사시보,
+    소방사,
+    소방교,
+    소방장,
+    소방위,
+    소방경,
+    소방령,
+    소방정,
+    소방준감,
+    소방감,
+    소방정감,
+    소방총감
 }
