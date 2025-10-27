cec14a331b27c5b37b2cf41d1d34195e7819bbe6
Ubuntu <ubuntu@ip-172-26-0-185.ap-northeast-2.compute.internal>
2025-10-27 07:39:03 +0000

    test: branch webhook test

--- BE/SSAIREN/src/main/java/com/ssairen/hello/HealthCheckController.java
+++ BE/SSAIREN/src/main/java/com/ssairen/hello/HealthCheckController.java
@@ -6,12 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-
-    @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
-    }
-
     @GetMapping("/")
     public ResponseEntity<String> home() {
         return ResponseEntity.ok("Hello World");
