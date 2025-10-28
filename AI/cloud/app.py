9ae50392066fbd1fccca4cb43d3245d273a01adf
DESKTOP-DLOPF19\SSAFY <tkddls0127@naver.com>
2025-10-28 13:26:58 +0900

    feat: stt router 생성

--- AI/cloud/app.py
+++ AI/cloud/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from routers.stt_router import router as stt_router
 
 app = FastAPI(title='CLOUD AI', description = "클라우드 AI를 기반으로 한 서비스를 제공하는 서버")
 
@@ -6,3 +7,4 @@ app = FastAPI(title='CLOUD AI', description = "클라우드 AI를 기반으로 �
 def read_root():
     return {"message": "Hello, Cloud AI!"}
 
+app.include_router(stt_router, prefix="/api")
