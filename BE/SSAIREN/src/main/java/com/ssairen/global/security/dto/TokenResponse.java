70af272069e0051fac12666b8e4e704c362d2cf6
DESKTOP-DLOPF19\SSAFY <roghk1204@naver.com>
2025-10-30 15:32:18 +0900

    refactor: TokenResponse 필드 순서 정리

--- BE/SSAIREN/src/main/java/com/ssairen/global/security/dto/TokenResponse.java
+++ BE/SSAIREN/src/main/java/com/ssairen/global/security/dto/TokenResponse.java
@@ -13,16 +13,16 @@ public record TokenResponse(
         @ExcludeFromLogging
         String refreshToken,
 
-        String tokenType,
         UserType userType,
         Integer userId,
-        String username
+        String username,
+        String tokenType
 ) {
     /**
      * 편의 생성자 (tokenType 기본값: Bearer)
      */
     public TokenResponse(String accessToken, String refreshToken, UserType userType,
                          Integer userId, String username) {
-        this(accessToken, refreshToken, "Bearer", userType, userId, username);
+        this(accessToken, refreshToken, userType, userId, username, "Bearer");
     }
 }
