5224784aa68a56e7720ab9f2035c342bbc637b36
yeongenn <yngn0012@naver.com>
2025-10-30 17:01:57 +0900

    feat: 출동지령별 구급일지 존재 여부 확인 쿼리 메서드 추가

--- BE/SSAIREN/src/main/java/com/ssairen/domain/emergency/repository/EmergencyReportRepository.java
+++ BE/SSAIREN/src/main/java/com/ssairen/domain/emergency/repository/EmergencyReportRepository.java
@@ -1,5 +1,6 @@
 package com.ssairen.domain.emergency.repository;
 
+import com.ssairen.domain.emergency.entity.Dispatch;
 import com.ssairen.domain.emergency.entity.EmergencyReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface EmergencyReportRepository extends JpaRepository<EmergencyReport, Long> {
+
+    /**
+     * 특정 출동지령에 대한 구급일지 존재 여부 확인
+     */
+    boolean existsByDispatch(Dispatch dispatch);
 }
