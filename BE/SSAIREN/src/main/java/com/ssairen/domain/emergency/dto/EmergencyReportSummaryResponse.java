79f8e4c1622fe314846758e1c763a153cd439acf
yeongenn <yngn0012@naver.com>
2025-11-15 22:42:03 +0900

    feat: 관내 보고서 조회 응답 필드 추가 (isCompleted, reporterName, patientName)

--- BE/SSAIREN/src/main/java/com/ssairen/domain/emergency/dto/EmergencyReportSummaryResponse.java
+++ BE/SSAIREN/src/main/java/com/ssairen/domain/emergency/dto/EmergencyReportSummaryResponse.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 
 public record EmergencyReportSummaryResponse(
         Long id,
+        Boolean isCompleted,
+        String patientName,
         ParamedicInfoSimple paramedicInfo,
         DispatchInfoSimple dispatchInfo,
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
